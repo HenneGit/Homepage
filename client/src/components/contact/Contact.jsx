527a18bfed57fab501bc6c5c981286167fc8735d
HenneGit <he.ahrens@gmail.com>
2022-11-23 14:35:00 +0100

    contact form finished and working

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './contact.css';
+import emailjs from '@emailjs/browser';
 
 
-const Form = () => (
-    <form id="contact-form">
-        <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name"/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
-            />
-        </div>
-        <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5" id="message"/>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-
-)
+export default class Contact extends Component {
+    constructor() {
+        super();
+    }
 
 
+    componentDidMount() {
+        init();
+
+        function init() {
+            emailjs.init('__dpeUhliQSxalKvl');
+            document.getElementById('contact-form').addEventListener('submit', submit);
+        }
 
 
-    function Contact() {
+        function submit(event) {
+            event.preventDefault();
+            // generate a five digit number for the contact_number variable
+            this.contact_number.value = Math.random() * 100000 | 0;
+            // these IDs from the previous steps
+            emailjs.sendForm('service_nutnlxd', 'template_wb0h6td', this)
+                .then(function () {
+                    saySomething("Thank you for your message :).");
+                }, function (error) {
+                    saySomething("Something went wrong :(.  Please try again later. ");
+                });
+        }
+
+        function saySomething(stringMessage) {
+            let formContainer = document.getElementById("form-container");
+            while (formContainer.firstChild) {
+                formContainer.removeChild(formContainer.firstChild);
+            }
+            let thankYou = document.createElement("h2");
+            thankYou.innerText = stringMessage;
+            formContainer.append(thankYou);
+
+        }
+
+
+    }
+
+    render() {
         return (
             <div id="form-container">
-                <Form/>
+                <div>
+                    <h1>Contact</h1>
+                    <form id="contact-form">
+                        <input type="hidden" name="contact_number"/>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" name="user_name"/>
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                               name="user_email"/>
+                        <label htmlFor="message">Message</label>
+                        <textarea className="form-control" rows="5" id="message" name="message"/>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
-        );
-    };
-
-export default Contact;
+        )
+    }
+}
